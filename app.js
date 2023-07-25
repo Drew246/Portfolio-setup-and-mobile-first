07f72ea25410028a9392ea2f601433cd7ae9fbba
Drew246 <andrew.k.mccollin@gmail.com>
2023-07-25 05:21:09 -0400

    add js file and toggle function

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggleMobileMenu() {
+    var mobileMenuList = document.getElementById("mobileMenuList");
+    mobileMenuList.classList.toggle("show");
+    
+    var mobileMenuButton = document.querySelector(".mobile-menu");
+    mobileMenuButton.classList.toggle("hidden");
+  }
+  
+  
