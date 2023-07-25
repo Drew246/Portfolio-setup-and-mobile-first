5aca497f58e24f7c5e7248be62d1f9750ae331f6
Drew246 <andrew.k.mccollin@gmail.com>
2023-07-25 09:11:17 -0400

    update menu funtionality

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 function toggleMobileMenu() {
     var mobileMenuList = document.getElementById("mobileMenuList");
-    mobileMenuList.classList.toggle("show");
-    
+    mobileMenuList.classList.toggle("show");    
     var mobileMenuButton = document.querySelector(".mobile-menu");
     mobileMenuButton.classList.toggle("hidden");
+
+}
+function closeMobileMenu() {
+    var mobileMenuList = document.getElementById("mobileMenuList");
+    mobileMenuList.classList.remove("show");
+    var mobileMenuButton = document.querySelector(".mobile-menu");
+    mobileMenuButton.classList.remove("hidden");
   }
-  
-  
+
+
